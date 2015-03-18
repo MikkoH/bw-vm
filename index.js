f4aa3fa64b560711a1f8a612381d284fe59a2c80
Mikko Haapoja <me@mikkoh.com>
2015-03-18 10:50:34 -0400

    Fixed issue where if on the command line sections were switched fast animateOut would not get called

--- index.js
+++ index.js
@@ -1,168 +1,187 @@
 function ViewManager( settings ) {
 
-	if( !( this instanceof ViewManager ) ) 
-		return new ViewManager( settings );
+  if( !( this instanceof ViewManager ) ) {
+    return new ViewManager( settings );
+  }
 
-	var s = this.s = settings || {};
+  var s = this.s = settings || {};
 
-	s.overlap = s.overlap === undefined ? true : s.overlap;
-	s.width = s.width || 980;
-	s.height = s.height || 570;
+  s.overlap = s.overlap === undefined ? true : s.overlap;
+  s.width = s.width || 980;
+  s.height = s.height || 570;
 
-	this.cContent = null;
-	this.nContent = null;
+  this.cContent = null;
+  this.nContent = null;
 }
 
 ViewManager.prototype = {
 
-	get overlap() {
+  get overlap() {
 
-		return this.s.overlap;
-	},
+    return this.s.overlap;
+  },
 
-	set overlap( value ) {
+  set overlap( value ) {
 
-		this.s.overlap = value;
-	},
+    this.s.overlap = value;
+  },
 
-	show: function( content, data, onComplete ) {
+  show: function( content, data, onComplete ) {
 
-		// check if data was passed in
-		if( onComplete === undefined &&
-			typeof data == 'function' ) {
+    // check if data was passed in
+    if( onComplete === undefined &&
+      typeof data == 'function' ) {
 
-			onComplete = data;
-			data = null;
-		}
+      onComplete = data;
+      data = null;
+    }
 
-		this.data = data;
+    this.data = data;
 
-		if( content != this.nContent && content != this.cContent ) {
+    if( content != this.nContent && content != this.cContent ) {
 
-			if( this.nContent && this.nContent.destroy )
-				this.nContent.destroy(this.data, function() { });
+      if( this.nContent && this.nContent.destroy )
+        this.nContent.destroy(this.data, function() { });
 
-			this.nContent = content;
+      this.nContent = content;
 
-			if( content.init ) {
+      if( content.init ) {
 
-				content.init( this.data, this.swap.bind( this, this.nContent, onComplete ) ); 
-			} else {
+        content.init( this.data, this.swap.bind( this, this.nContent, onComplete ) ); 
+      } else {
 
-				this.swap( this.nContent, onComplete );
-			}
-		}
-	},
+        this.swap( this.nContent, onComplete );
+      }
+    }
+  },
 
-	clear: function( onComplete ) {
+  clear: function( onComplete ) {
 
-		if( this.nContent && this.nContent.destroy )
-			this.nContent.destroy( this.data, function() { } );
+    if( this.nContent && this.nContent.destroy ) {
+      this.nContent.destroy( this.data, function() { } );
+    }
 
-		if( this.cContent ) {
+    if( this.cContent ) {
 
-			var onOldOut = function( oldContent ) {
+      var onOldOut = function( oldContent ) {
 
-				if( oldContent.destroy )
-					oldContent.destroy( this.data , function() { } );
+        if( oldContent.destroy ) {
+          oldContent.destroy( this.data , function() { } );
+        }
 
-				if( onComplete )
-					onComplete( oldContent );
-			}.bind( this, this.cContent );
+        if( onComplete ) {
+          onComplete( oldContent );
+        }
+      }.bind( this, this.cContent );
 
-			// now take out countent
-			if( this.cContent.animateOut )
-				this.cContent.animateOut( this.data , onOldOut );
-			else
-				onOldOut();
-		}
-	},
+      // now take out countent
+      if( this.cContent.animateOut ) {
+        this.cContent.animateOut( this.data , onOldOut );
+      } else {
+        onOldOut();
+      }
+    }
+  },
 
-	resize: function( width, height ) {
+  resize: function( width, height ) {
 
-		var s = this.s;
+    var s = this.s;
 
-		s.width = width;
-		s.height = height;
+    s.width = width;
+    s.height = height;
 
-		if( this.cContent && this.cContent.resize )
-			this.cContent.resize( width, height );
-	},
+    if( this.cContent && this.cContent.resize )
+      this.cContent.resize( width, height );
+  },
 
-	swap: function( newContent, onComplete ) {
+  swap: function( newContent, onComplete ) {
 
-		if( newContent == this.nContent ) {
+    if( newContent == this.nContent ) {
 
-			var s = this.s,
-				oldContent = this.cContent,
-				onNewIn = function() {
+      var s = this.s;
+      var oldContent = this.cContent;
+      var onNewIn = function() {
 
-					if( s.onEndAniIn )
-						s.onEndAniIn( newContent, oldContent );
+        if( s.onEndAniIn ) {
+          s.onEndAniIn( newContent, oldContent );
+        }
 
-					if( onComplete )
-						onComplete( newContent, oldContent );
-				},
-				onOldOut;
+        if( onComplete ) {
+          onComplete( newContent, oldContent );
+        }
+      };
+      var onOldOut;
 
-			// resize the newContent if it has a resize method
-			if( newContent.resize )
-				newContent.resize( s.width, s.height );
+      // resize the newContent if it has a resize method
+      if( newContent.resize ) {
+        newContent.resize( s.width, s.height );
+      }
 
-			// call a callback that we're starting to animatein content
-			if( s.onStartAniIn ) 
-				s.onStartAniIn( newContent, this.cContent );
+      // call a callback that we're starting to animatein content
+      if( s.onStartAniIn ) {
+        s.onStartAniIn( newContent, this.cContent );
+      }
 
-			// check if there's content on screen already
-			if( this.cContent ) {
+      // check if there's content on screen already
+      if( this.cContent ) {
 
-				// setup the old out function
-				onOldOut = function() {
+        this.cContent = newContent;
+        this.nContent = null;
 
-					if( s.onEndAniOut )
-						s.onEndAniOut( newContent, oldContent );
+        // setup the old out function
+        onOldOut = function() {
 
-					if( oldContent.destroy )
-						oldContent.destroy( this.data, function() { } );
+          if( s.onEndAniOut ) {
+            s.onEndAniOut( newContent, oldContent );
+          }
 
-					if( !s.overlap ) {
+          if( oldContent.destroy ) {
+            oldContent.destroy( this.data, function() { } );
+          }
 
-						if( newContent.animateIn )
-							newContent.animateIn( this.data, onNewIn );
-						else
-							onNewIn();
-					}
-				}.bind(this);
+          if( !s.overlap ) {
 
-				// call a callback that we're starting to animateout content
-				if( s.onStartAniOut )
-					s.onStartAniOut( newContent, oldContent );
+            if( newContent.animateIn ) {
+              newContent.animateIn( this.data, onNewIn );
+            } else {
+              onNewIn();
+            }
+          }
+        }.bind(this);
 
-				if( oldContent.animateOut )
-					oldContent.animateOut( this.data, onOldOut );
-				else
-					onOldOut();
+        // call a callback that we're starting to animateout content
+        if( s.onStartAniOut ) {
+          s.onStartAniOut( newContent, oldContent );
+        }
 
-				if( s.overlap ) {
+        if( oldContent.animateOut ) {
+          oldContent.animateOut( this.data, onOldOut );
+        } else {
+          onOldOut();
+        }
 
-					if( newContent.animateIn )
-						newContent.animateIn( this.data, onNewIn );
-					else
-						onNewIn();
-				}
-			} else {
+        if( s.overlap ) {
 
-				// just bring new content
-				if( newContent.animateIn )
-					newContent.animateIn( this.data, onNewIn );
-				else
-					onNewIn();
-			}
+          if( newContent.animateIn ) {
+            newContent.animateIn( this.data, onNewIn );
+          } else {
+            onNewIn();
+          }
+        }
+      } else {
 
-			this.cContent = newContent;
-			this.nContent = null;
-		}
-	}
+        this.cContent = newContent;
+        this.nContent = null;
+
+        // just bring new content
+        if( newContent.animateIn ) {
+          newContent.animateIn( this.data, onNewIn );
+        } else {
+          onNewIn();
+        }
+      }
+    }
+  }
 };
 
 module.exports = ViewManager;
